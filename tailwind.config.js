bd82cb491df28553ba4d2437a81dc6543e374b34
daniellauding <daniel@lauding.se>
2022-01-29 10:23:07 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            screens: {
+                light: { raw: "(prefers-color-scheme: light)" },
+                dark: { raw: "(prefers-color-scheme: dark)" }
+            }
+        },
+        // fontFamily: {
+        //     'sans': ['Roboto', 'sans-serif'],
+        //     'serif': ['sans-serif', 'ui-serif', 'Georgia'],
+        //     'mono': ['ui-monospace', 'SFMono-Regular'],
+        //     'display': ['Roboto', 'sans-serif'],
+        //     'body': ['Roboto', 'sans-serif'],
+        // }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        function({ addBase, config }) {
+            addBase({
+                body: {
+                    color: config("theme.colors.black"),
+                    backgroundColor: config("theme.colors.white")
+                },
+                "@screen dark": {
+                    body: {
+                        color: config("theme.colors.white"),
+                        backgroundColor: config("theme.colors.black")
+                    }
+                }
+            });
+        }
+    ],
+}
