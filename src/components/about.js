97ea1fc590f50520c43bfc8edffef9011119c296
Your Name <daniel@lauding.se>
2023-01-11 12:49:44 +0100

    node version

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import classNames from 'classnames';
 import Avatar from './avatar';
 import Profile from './profile';
