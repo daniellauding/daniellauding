389b6dc61a88872e2508ce04d0aed6169487adc4
daniellauding <daniel@lauding.se>
2023-07-31 19:34:58 +0200

    added fetching and react

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const ContactForm = () => {
+const ContactForm = ({ onClose }) => {
 	const [formState, setFormState] = useState({
 		name: '',
 		company: '',
@@ -45,79 +45,82 @@ const ContactForm = () => {
 	};
 
 	return (
-		<form
-			name="contact"
-			method="post"
-			data-netlify="true"
-			onSubmit={handleSubmit}
-		>
-			<input type="hidden" name="contact" value="contact" />
-			<p>
-				<label>
-					Your Name:
-					<input
-						type="text"
-						name="name"
-						placeholder="Full name"
-						value={formState.name}
-						onChange={handleChange}
-						className="border-b border-b-2 dark:bg-transparent border-gray-300 hover:border-gray-600 active:border-gray-800 focus:border-gray-800 outline-0 py-3 w-full text-black"
-					/>
-				</label>
-			</p>
-			<p>
-				<label>
-					Your Company:
-					<input
-						type="text"
-						name="company"
-						placeholder="Company name"
-						value={formState.company}
-						onChange={handleChange}
-						className="border-b border-b-2 dark:bg-transparent border-gray-300 hover:border-gray-600 active:border-gray-800 focus:border-gray-800 outline-0 py-3 w-full text-black"
-					/>
-				</label>
-			</p>
-			<p>
-				<label>
-					Your Email:
-					<input
-						type="email"
-						name="email"
-						placeholder="E-mail"
-						value={formState.email}
-						onChange={handleChange}
-						className="border-b border-b-2 dark:bg-transparent border-gray-300 hover:border-gray-600 active:border-gray-800 focus:border-gray-800 outline-0 py-3 w-full text-black"
-					/>
-				</label>
-			</p>
-			<p>
-				<label>
-					Message:
-					<textarea
-						name="message"
-						placeholder="I want to see more about the project ${item} please send me a code"
-						value={formState.message}
-						onChange={handleChange}
-						className="border-b border-b-2 dark:bg-transparent border-gray-300 hover:border-gray-600 active:border-gray-800 focus:border-gray-800 outline-0 py-3 w-full text-black"
-					/>
-				</label>
-			</p>
-			<p>
-				<button
-					type="submit"
-					disabled={submitting}
-					className="bg-primary hover:primary text-white font-bold py-5 px-8 mt-8 rounded-full cursor-pointer"
-				>
-					Request access
-				</button>
-			</p>
-			{submitted && (
+		<div>
+			{submitted ? (
 				<p className="pt-0 mb-8 ml-0 text-left text-2xl dark:text-gray-500 text-black lg:font-light">
 					Thank you for contacting us!
 				</p>
+			) : (
+				<form
+					name="contact"
+					method="post"
+					data-netlify="true"
+					onSubmit={handleSubmit}
+				>
+					<input type="hidden" name="contact" value="contact" />
+					<p>
+						<label>
+							Your Name:
+							<input
+								type="text"
+								name="name"
+								placeholder="Full name"
+								value={formState.name}
+								onChange={handleChange}
+								className="border-b border-b-2 dark:bg-transparent border-gray-300 hover:border-gray-600 active:border-gray-800 focus:border-gray-800 outline-0 py-3 w-full text-black"
+							/>
+						</label>
+					</p>
+					<p>
+						<label>
+							Your Company:
+							<input
+								type="text"
+								name="company"
+								placeholder="Company name"
+								value={formState.company}
+								onChange={handleChange}
+								className="border-b border-b-2 dark:bg-transparent border-gray-300 hover:border-gray-600 active:border-gray-800 focus:border-gray-800 outline-0 py-3 w-full text-black"
+							/>
+						</label>
+					</p>
+					<p>
+						<label>
+							Your Email:
+							<input
+								type="email"
+								name="email"
+								placeholder="E-mail"
+								value={formState.email}
+								onChange={handleChange}
+								className="border-b border-b-2 dark:bg-transparent border-gray-300 hover:border-gray-600 active:border-gray-800 focus:border-gray-800 outline-0 py-3 w-full text-black"
+							/>
+						</label>
+					</p>
+					<p>
+						<label>
+							Message:
+							<textarea
+								name="message"
+								placeholder="I want to see more about the project ${item} please send me a code"
+								value={formState.message}
+								onChange={handleChange}
+								className="border-b border-b-2 dark:bg-transparent border-gray-300 hover:border-gray-600 active:border-gray-800 focus:border-gray-800 outline-0 py-3 w-full text-black"
+							/>
+						</label>
+					</p>
+					<p>
+						<button
+							type="submit"
+							disabled={submitting || submitted}
+							className="bg-primary hover:primary text-white font-bold py-5 px-8 mt-8 rounded-full cursor-pointer"
+						>
+							Request access
+						</button>
+					</p>
+				</form>
 			)}
-		</form>
+		</div>
 	);
 };
 
